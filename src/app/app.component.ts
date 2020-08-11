c18b3e1b427504c1171ba7952456ccb2d0d8d774
Dakshayani Ramachandrappa <dakshayani_r@hna.honda.com>
2020-08-11 20:06:16 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'appComponent';
+  details = [
+    {
+      id: 1,
+      title: 'Honda Accord',
+      type: 'TCU',
+      make: 'Honda',
+      model: 'Accord'
+    },
+    {
+      id: 2,
+      title: 'Acura TLX',
+      type: 'Non-TCU',
+      make: 'Acura',
+      model: 'TLX'
+    },
+    {
+      id: 3,
+      title: 'Honda CR-V',
+      type: 'TCU',
+      make: 'Honda',
+      model: 'CR-V'
+    }
+  ];
 }
